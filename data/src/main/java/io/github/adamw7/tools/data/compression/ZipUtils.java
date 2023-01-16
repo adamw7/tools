c4b53479ef3097cbf2ed159a808522a9d764a9d0
adamw7 <adamwitkowski3@gmail.com>
2023-01-16 09:16:30 +0100

    Switching zip detection from file name matching to binary content check

--- data/src/main/java/io/github/adamw7/tools/data/compression/ZipUtils.java
+++ data/src/main/java/io/github/adamw7/tools/data/compression/ZipUtils.java
@@ -1,12 +1,19 @@
 package io.github.adamw7.tools.data.compression;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
 import java.util.zip.GZIPInputStream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ZipUtils {
 
-	public static InputStream unzipIfNeeded(InputStream stream, String fileName, String regex) {
-		if (isZipped(fileName, regex) && !(stream instanceof GZIPInputStream)) {
+	private final static Logger log = LogManager.getLogger(ZipUtils.class.getName());
+
+	public static InputStream unzipIfNeeded(InputStream stream, String fileName) {
+		if (isZipped(fileName) && !(stream instanceof GZIPInputStream)) {
 			try {
 				return new GZIPInputStream(stream);
 			} catch (Exception e) {
@@ -17,12 +24,20 @@ public class ZipUtils {
 		}
 	}
 
-	public static InputStream unzipIfNeeded(InputStream stream, String fileName) {
-		return unzipIfNeeded(stream, fileName, "");
-	}
+	private static boolean isZipped(String fileName) {
+		RandomAccessFile raf;
+		try {
+			raf = new RandomAccessFile(fileName, "r");
+			byte first = raf.readByte();
+			byte second = raf.readByte();
 
-	private static boolean isZipped(String fileName, String regex) {
-		return fileName.matches(regex) || fileName.toLowerCase().endsWith("gz")
-				|| fileName.toLowerCase().endsWith("zip");
+			raf.close();
+
+			return ((first == (byte) (GZIPInputStream.GZIP_MAGIC))
+					&& (second == (byte) (GZIPInputStream.GZIP_MAGIC >> 8)));
+		} catch (IOException e) {
+			log.error("Error checking if " + fileName + " is zipped", e);
+			return false;
+		}
 	}
 }

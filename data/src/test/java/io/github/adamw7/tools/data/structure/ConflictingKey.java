b556ebfb273e3839f25a6dd7687a4f8be12cb59c
adamw7 <adamwitkowski3@gmail.com>
2023-02-12 15:40:33 +0100

    Adding forgotten class

---
+++ data/src/test/java/io/github/adamw7/tools/data/structure/ConflictingKey.java
@@ -0,0 +1,31 @@
+package io.github.adamw7.tools.data.structure;
+
+import java.util.Objects;
+
+public class ConflictingKey {
+
+	private int number;
+	private String string;
+	
+	public ConflictingKey(int number, String string) {
+		this.number = number;
+		this.string = string;
+	}
+	
+	@Override
+	public int hashCode() {
+		return 1; // conflicting hash
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ConflictingKey other = (ConflictingKey) obj;
+		return number == other.number && Objects.equals(string, other.string);
+	}
+}

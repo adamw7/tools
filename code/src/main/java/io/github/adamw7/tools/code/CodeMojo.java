4a7e52b4c2827899d25f0216600f5ddb02eee8a4
adamw7 <adamwitkowski3@gmail.com>
2023-02-14 15:55:32 +0100

    Introducing an empty project for maven plugin for code generation

---
+++ code/src/main/java/io/github/adamw7/tools/code/CodeMojo.java
@@ -0,0 +1,20 @@
+package io.github.adamw7.tools.code;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "code-generator")
+public class CodeMojo extends AbstractMojo {
+	
+	private final static Logger log = LogManager.getLogger(CodeMojo.class.getName());
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		log.info("Executing " + this + " maven plugin");
+	}
+
+}

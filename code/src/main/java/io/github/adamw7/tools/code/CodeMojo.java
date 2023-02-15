1fa5c7761bbacfe7f980d65e78c6d006e05e9ed8
adamw7 <adamwitkowski3@gmail.com>
2023-02-15 09:59:51 +0100

    Adding life cycle phase to maven plugin

--- code/src/main/java/io/github/adamw7/tools/code/CodeMojo.java
+++ code/src/main/java/io/github/adamw7/tools/code/CodeMojo.java
@@ -5,9 +5,10 @@ import org.apache.logging.log4j.Logger;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 
-@Mojo(name = "code-generator")
+@Mojo(name = "code-generator", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class CodeMojo extends AbstractMojo {
 	
 	private final static Logger log = LogManager.getLogger(CodeMojo.class.getName());

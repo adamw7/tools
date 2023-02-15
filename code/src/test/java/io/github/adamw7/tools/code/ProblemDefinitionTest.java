b8a184c114f2c85b0ce5d3bbd31f4f0c7f358b89
adamw7 <adamwitkowski3@gmail.com>
2023-02-15 13:23:12 +0100

    Introducing generation java from proto plus a test demonstrating a problem to solve

---
+++ code/src/test/java/io/github/adamw7/tools/code/ProblemDefinitionTest.java
@@ -0,0 +1,28 @@
+package io.github.adamw7.tools.code;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.google.protobuf.UninitializedMessageException;
+
+import io.github.adamw7.tools.code.protos.Person;
+
+public class ProblemDefinitionTest {
+
+	@Test
+	public void demonstrateRequiredFieldIssue() {
+		Person.Builder personBuilder = Person.newBuilder();
+
+		personBuilder.setEmail("email@sth.com");
+		personBuilder.setName("Adam");
+
+		UninitializedMessageException thrown = assertThrows(UninitializedMessageException.class, () -> {
+			personBuilder.build();
+		}, "Expected build method to throw, but it didn't");
+
+		assertEquals("Message missing required fields: id", thrown.getMessage());
+
+	}
+}

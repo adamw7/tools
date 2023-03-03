3a7e0c2bec29c681bee462df5abb22d04375fdb7
adamw7 <adamwitkowski3@gmail.com>
2023-03-03 09:24:03 +0100

    Adding basic tests for generated directories and files

--- code/protogen-maven-plugin/src/test/java/io/github/adamw7/tools/code/CodeMojoTest.java
+++ code/protogen-maven-plugin/src/test/java/io/github/adamw7/tools/code/CodeMojoTest.java
@@ -1,5 +1,9 @@
 package io.github.adamw7.tools.code;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
 
 public class CodeMojoTest {
@@ -7,5 +11,9 @@ public class CodeMojoTest {
 	@Test
 	public void happyPath() {
 		new CodeMojo().execute();
+		File outputDir = new File("target/generated-sources/io.github.adamw7.tools.code/");
+		assertTrue(outputDir.exists());
+		File generatedClassFile = new File(outputDir.getAbsolutePath() + "/PersonBuilder.java"); 
+		assertTrue(generatedClassFile.exists());
 	}
 }

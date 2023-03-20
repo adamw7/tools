4c7d15bc7c2ff727535881a0185ac92709b4280e
adamw7 <adamwitkowski3@gmail.com>
2023-03-20 11:55:01 +0100

    Refactoring code generation

--- code/protogen-maven-plugin/src/main/java/io/github/adamw7/tools/code/Clazz.java
+++ code/protogen-maven-plugin/src/main/java/io/github/adamw7/tools/code/Clazz.java
@@ -14,18 +14,19 @@ public class Clazz {
 	private final String className;
 	private final TypeMappings typeMappings;
 	private final List<FieldDescriptor> requiredFields;
-	private final List<FieldDescriptor> optionalFields;	
 	private final String pkg;
 	private final Interfaces interfaces;
+	private final Implementations implementations;
 	 
 	public Clazz(Descriptor descriptor, TypeMappings typeMappings, Package pkg) {
 		this.descriptor = descriptor;
 		className = getClassName();
 		requiredFields = getRequiredFields();
-		optionalFields = getOptionalFields();
+		List<FieldDescriptor> optionalFields = getOptionalFields();
 		this.pkg = pkg.getName();	
 		this.typeMappings = typeMappings;
 		this.interfaces = new Interfaces(className, optionalFields, requiredFields, typeMappings);
+		this.implementations = new Implementations(className, optionalFields, requiredFields, typeMappings);
 	}
 
 	private List<FieldDescriptor> getRequiredFields() {
@@ -45,11 +46,11 @@ public class Clazz {
 		StringBuilder imports = generateImports();
 		StringBuilder requiredInterfaces = interfaces.generateRequired();	
 		StringBuilder optionalInterface = interfaces.generateOptional();
-		StringBuilder requiredImpl = generateRequiredImpl();
+		StringBuilder requiredImpl = implementations.generateRequired();
 		
 		StringBuilder header = generateHeader();
 		StringBuilder fields = generateFields();	
-		StringBuilder optionalImpl = generateOptionalImpl();	
+		StringBuilder optionalImpl = implementations.generateOptional();	
 		
 		StringBuilder footer = generateFooter();
 		
@@ -73,60 +74,6 @@ public class Clazz {
 			setter.append("\t\treturn new ").append(Utils.getNext(requiredFields, field, "Impl")).append("(personBuilder);\n\t}\n");
 			return setter;
 		}
-	}
-	
-	private StringBuilder generateRequiredImpl() {
-		StringBuilder builder = new StringBuilder();
-		if (requiredFields.size() > 1) {
-			for (int i = 1; i < requiredFields.size(); ++i) { // skipping first since already handled
-				String classOrBuilder = Utils.firstToLower(className) + "OrBuilder";
-				FieldDescriptor field = requiredFields.get(i);
-				String ifcName = Utils.firstToUpper(field.getName()) + "Ifc";
-				String implName = Utils.firstToUpper(field.getName()) + "Impl";
-				builder.append("class ").append(implName).append(" implements ").append(ifcName).append(" {\n");
-				builder.append("\tprivate final Builder ").append(classOrBuilder).append(";\n");
-				builder.append("\tpublic ").append(implName).append("(Builder ").append(classOrBuilder);
-				builder.append(") {\n");
-				builder.append("\t\tthis.").append(classOrBuilder).append(" = ").append(classOrBuilder).append(";\n\t}\n");
-				builder.append(generateRequiredSetter(classOrBuilder, field));
-				builder.append("\n}\n");
-			}
-		}
-		return builder;
-	}
-
-	private StringBuilder generateRequiredSetter(String classOrBuilder, FieldDescriptor field) {
-		StringBuilder builder = new StringBuilder("\t@Override\n");
-		builder.append("\tpublic ").append(Utils.getNext(requiredFields, field, "Ifc")).append(" ").append(Utils.generateSetter(field, typeMappings)).append(" {\n");
-		builder.append("\t\t").append(classOrBuilder).append(".set").append(Utils.firstToUpper(field.getName()));
-		builder.append("(").append(field.getName()).append(");\n");
-		builder.append("\t\treturn new ").append(Utils.getNext(requiredFields, field, "Impl")).append("(").append(classOrBuilder).append(");\n");
-		builder.append("\t}");
-		return builder;
-	}
-
-	private StringBuilder generateOptionalImpl() {
-		StringBuilder builder = new StringBuilder("class OptionalImpl implements OptionalIfc {\n");
-		builder.append("\tprivate final Builder builder;\n");
-		builder.append("\tpublic OptionalImpl(Builder builder) {\n");
-		builder.append("\t\tthis.builder = builder;\n\t\t}\n");
-		builder.append(generateSetters());
-		builder.append("\t@Override\n").append("\t\tpublic ").append(className).append(" build() {\n");
-		builder.append("\t\treturn builder.build();\n\t\t}\n\t}\n");
-		return builder;
-	}
-
-	private StringBuilder generateSetters() {
-		StringBuilder builder = new StringBuilder();
-		for (FieldDescriptor field : optionalFields) {
-			builder.append("\t\t@Override\n");
-			builder.append("\t\tpublic OptionalIfc ");
-			builder.append(Utils.generateSetter(field, typeMappings)).append(" {\n");
-			builder.append("\t\t\tbuilder.set").append(Utils.firstToUpper(field.getName()));
-			builder.append("(").append(field.getName()).append(");\n");
-			builder.append("\t\t\treturn this;\n\t\t}\n");
-		}
-		return builder;
 	}
 
 	private StringBuilder generatePackage() {

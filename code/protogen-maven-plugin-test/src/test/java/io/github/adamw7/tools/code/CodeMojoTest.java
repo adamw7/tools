8d563b5c0bd3be50911f24da8dc610e2c798b0f3
adamw7 <adamwitkowski3@gmail.com>
2023-03-22 10:48:02 +0100

    Moving the test checking the existence of generated files to protogen-maven-plugin-test

--- code/protogen-maven-plugin-test/src/test/java/io/github/adamw7/tools/code/CodeMojoTest.java
+++ code/protogen-maven-plugin-test/src/test/java/io/github/adamw7/tools/code/CodeMojoTest.java
@@ -4,15 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 public class CodeMojoTest {
 
 	@Test
 	public void happyPath() {
-		new CodeMojo().execute();
 		File outputDir = new File("target/generated-sources/io/github/adamw7/tools/code/");
 		assertTrue(outputDir.exists());
 		File generatedClassFile = new File(outputDir.getAbsolutePath() + "/PersonBuilder.java"); 

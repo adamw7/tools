fbef62d7ff0fdcf0c8e6c2ee08f7700d997c1c3f
adamw7 <adamwitkowski3@gmail.com>
2023-03-23 13:56:37 +0100

    Introducing test that is executing generated code

--- code/protogen-maven-plugin-test/src/test/java/io/github/adamw7/tools/code/CodeGenerationTest.java
+++ code/protogen-maven-plugin-test/src/test/java/io/github/adamw7/tools/code/CodeGenerationTest.java
@@ -1,10 +1,13 @@
 package io.github.adamw7.tools.code;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 
 import org.junit.jupiter.api.Test;
+
+import io.github.adamw7.tools.code.protos.Person;
 
 public class CodeGenerationTest {
 
@@ -22,5 +25,15 @@ public class CodeGenerationTest {
 		assertTrue(outputDir.exists());
 		File generatedClassFile = new File(outputDir.getAbsolutePath() + "/PersonBuilder.class"); 
 		assertTrue(generatedClassFile.exists());
+	}
+	
+	@Test
+	public void codeExecutes() {
+		PersonBuilder builder = new PersonBuilder();
+		Person person = builder.setId(1).setDepartment("dep").setEmail("sth@sth.net").setName("Adam").build();
+		assertEquals(1, person.getId());
+		assertEquals("dep", person.getDepartment());
+		assertEquals("sth@sth.net", person.getEmail());
+		assertEquals("Adam", person.getName());
 	}
 }

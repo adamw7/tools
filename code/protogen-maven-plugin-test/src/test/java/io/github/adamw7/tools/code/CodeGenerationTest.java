aa3309865bd2c5b439292ae42d2777866224f208
adamw7 <adamwitkowski3@gmail.com>
2023-04-06 11:03:33 +0200

    Introducing asserts for methods in generated interfaces

--- code/protogen-maven-plugin-test/src/test/java/io/github/adamw7/tools/code/CodeGenerationTest.java
+++ code/protogen-maven-plugin-test/src/test/java/io/github/adamw7/tools/code/CodeGenerationTest.java
@@ -1,14 +1,12 @@
 package io.github.adamw7.tools.code;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.io.File;
 
 import org.junit.jupiter.api.Test;
 
 import io.github.adamw7.tools.code.protos.Person;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CodeGenerationTest {
 
@@ -31,30 +29,27 @@ public class CodeGenerationTest {
 	@Test
 	public void codeExecutes() {
 		PersonBuilder builder = new PersonBuilder();
-		Person person = builder.setId(1).setDepartment("dep").setEmail("sth@sth.net").setName("Adam").
+		assertFalse(builder.hasId());
+		DepartmentIfc department = builder.setId(1);
+		assertTrue(builder.hasId());
+		assertFalse(department.hasDepartment());
+		PersonOptionalIfc optional = department.setDepartment("dep");
+		assertTrue(department.hasDepartment());
+		Person person = optional.setEmail("sth@sth.net").setName("Adam").
 				setSalary(1000L).setFactor(0f).setGender(1).setPhone(12345678L).setLevel(6).
 				setGrade(10L).setUnit(30).setExternalId(500L).setActive(true).setLocation(17).
 				setCooridantes(9999999L).setPercent(0.4).build();
-		
+
+		assertOptional(optional);
+
 		assertTrue(builder.hasId());
-		
-		assertTrue(person.hasId());
-		assertTrue(person.hasDepartment());
-		assertTrue(person.hasEmail());
-		assertTrue(person.hasName());
-		assertTrue(person.hasSalary());
-		assertTrue(person.hasFactor());
-		assertTrue(person.hasGender());	
-		assertTrue(person.hasPhone());		
-		assertTrue(person.hasLevel());
-		assertTrue(person.hasGrade());
-		assertTrue(person.hasUnit());
-		assertTrue(person.hasExternalId());
-		assertTrue(person.hasActive());
-		assertTrue(person.hasLocation());
-		assertTrue(person.hasCooridantes());
-		assertTrue(person.hasPercent());
-		
+
+		assertPersonFieldsAreSet(person);
+
+		assertPersonFieldsHaveProperValues(person);
+	}
+
+	private static void assertPersonFieldsHaveProperValues(Person person) {
 		assertEquals(1, person.getId());
 		assertEquals("dep", person.getDepartment());
 		assertEquals("sth@sth.net", person.getEmail());
@@ -67,9 +62,45 @@ public class CodeGenerationTest {
 		assertEquals(10L, person.getGrade());
 		assertEquals(30, person.getUnit());
 		assertEquals(500L, person.getExternalId());
-		assertEquals(true, person.getActive());		
-		assertEquals(17, person.getLocation());	
-		assertEquals(9999999L, person.getCooridantes());			
-		assertEquals(0.4, person.getPercent());					
+		assertEquals(true, person.getActive());
+		assertEquals(17, person.getLocation());
+		assertEquals(9999999L, person.getCooridantes());
+		assertEquals(0.4, person.getPercent());
+	}
+
+	private static void assertPersonFieldsAreSet(Person person) {
+		assertTrue(person.hasId());
+		assertTrue(person.hasDepartment());
+		assertTrue(person.hasEmail());
+		assertTrue(person.hasName());
+		assertTrue(person.hasSalary());
+		assertTrue(person.hasFactor());
+		assertTrue(person.hasGender());
+		assertTrue(person.hasPhone());
+		assertTrue(person.hasLevel());
+		assertTrue(person.hasGrade());
+		assertTrue(person.hasUnit());
+		assertTrue(person.hasExternalId());
+		assertTrue(person.hasActive());
+		assertTrue(person.hasLocation());
+		assertTrue(person.hasCooridantes());
+		assertTrue(person.hasPercent());
+	}
+
+	private static void assertOptional(PersonOptionalIfc optional) {
+		assertTrue(optional.hasEmail());
+		assertTrue(optional.hasName());
+		assertTrue(optional.hasSalary());
+		assertTrue(optional.hasFactor());
+		assertTrue(optional.hasGender());
+		assertTrue(optional.hasPhone());
+		assertTrue(optional.hasLevel());
+		assertTrue(optional.hasGrade());
+		assertTrue(optional.hasUnit());
+		assertTrue(optional.hasExternalId());
+		assertTrue(optional.hasActive());
+		assertTrue(optional.hasLocation());
+		assertTrue(optional.hasCooridantes());
+		assertTrue(optional.hasPercent());
 	}
 }

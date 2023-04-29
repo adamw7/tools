3a33f39f0e2c103171c618de8791834e2655deea
adamw7 <adamwitkowski3@gmail.com>
2023-04-29 12:38:29 +0200

    More test cases in Utils tests

--- code/protogen-maven-plugin/src/test/java/io/github/adamw7/tools/code/gen/UtilsTest.java
+++ code/protogen-maven-plugin/src/test/java/io/github/adamw7/tools/code/gen/UtilsTest.java
@@ -10,18 +10,27 @@ public class UtilsTest {
 	public void toUpperCamelCase() {
 		String camelCase = Utils.toUpperCamelCase("external_id");
 		assertEquals("ExternalId", camelCase);
+		
+		String multiCamelCase = Utils.toUpperCamelCase("external_id_by_name");
+		assertEquals("ExternalIdByName", multiCamelCase);
 	}
 	
 	@Test
 	public void firstToLower() {
 		String firstToLower = Utils.firstToLower("Builder");
 		assertEquals("builder", firstToLower);
+		
+		String shouldNotChange = Utils.firstToLower("builder");
+		assertEquals("builder", shouldNotChange);
 	}
 	
 	@Test
 	public void suffix() {
-		String suffix = Utils.getSuffixOf("pkg.EnumType.field", 2, ".");
-		assertEquals("EnumType.field", suffix);
+		String doubleSuffix = Utils.getSuffixOf("pkg.EnumType.field", 2, ".");
+		assertEquals("EnumType.field", doubleSuffix);
+		
+		String singleSuffix = Utils.getSuffixOf("pkg,EnumType,field", 1, ",");
+		assertEquals("field", singleSuffix);
 	}
 
 }

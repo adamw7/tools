fa0981ff4b854d87409691313b7e5fb831543dff
adamw7 <adamwitkowski3@gmail.com>
2023-05-02 14:53:45 +0200

    Introducing maven Mojo level test

---
+++ code/protogen-maven-plugin/src/test/java/io/github/adamw7/tools/code/MojoTest.java
@@ -0,0 +1,35 @@
+package io.github.adamw7.tools.code;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class MojoTest {
+
+	protected static String GENERATED_SOURCES;
+	
+	@BeforeAll
+	public static void setupPath() {
+		String target = new File(MojoTest.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent();
+		GENERATED_SOURCES = target + "/generated-sources/";
+	}
+	
+	@Test
+	public void happyPath() {
+		CodeMojo mojo = new CodeMojo();
+		mojo.generatedSourcesDir = GENERATED_SOURCES;
+		mojo.execute();
+	}
+	
+	@AfterEach
+	public void cleanUp() throws IOException {
+		File dir = new File(GENERATED_SOURCES);
+		dir.delete();
+	}
+	
+}

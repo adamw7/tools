44181eb5291a10b068ece1e62a6717722db71f89
adamw7 <adamwitkowski3@gmail.com>
2023-05-23 15:40:56 +0200

    Next iterations of unhardcoding the generated package

--- code/protogen-maven-plugin/src/main/java/io/github/adamw7/tools/code/CodeMojo.java
+++ code/protogen-maven-plugin/src/main/java/io/github/adamw7/tools/code/CodeMojo.java
@@ -17,14 +17,19 @@ import io.github.adamw7.tools.code.gen.Code;
 public class CodeMojo extends AbstractMojo {
 	
 	private final static Logger log = LogManager.getLogger(CodeMojo.class.getName());
-
+	
+	public final static String DEFAULT_PACKAGE = "io.github.adamw7.tools.code.protos";
+	
 	@Parameter(property = "generatedsourcesdir", required = true)
 	protected String generatedSourcesDir;
+	
+	@Parameter(property = "pkg", required = false, defaultValue = DEFAULT_PACKAGE)
+	protected String pkg;
 	
 	@Override
 	public void execute() {
 		log.info("Executing {} maven plugin", this);
-		Set<Class<? extends GeneratedMessageV3>> allMessages = new MessagesFinder("io.github.adamw7.tools.code.protos").execute();
+		Set<Class<? extends GeneratedMessageV3>> allMessages = new MessagesFinder(pkg).execute();
 		new Code(generatedSourcesDir).genBuilders(allMessages);
 	}
 

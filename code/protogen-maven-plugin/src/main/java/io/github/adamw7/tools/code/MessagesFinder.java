748421b48a8b97d47bd6fcaf7ff5e6f6f8182ebf
adamw7 <adamwitkowski3@gmail.com>
2023-06-15 09:48:07 +0200

    Improving the way refections are looking for classes

--- code/protogen-maven-plugin/src/main/java/io/github/adamw7/tools/code/MessagesFinder.java
+++ code/protogen-maven-plugin/src/main/java/io/github/adamw7/tools/code/MessagesFinder.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.reflections.Reflections;
+import org.reflections.util.ConfigurationBuilder;
 
 import com.google.protobuf.GeneratedMessageV3;
 
@@ -18,7 +19,8 @@ public class MessagesFinder {
 	}
 	
 	public Set<Class<? extends GeneratedMessageV3>> execute() {
-		Reflections reflections = new Reflections(pkg);
+		 Reflections reflections =
+			        new Reflections(new ConfigurationBuilder().forPackages(pkg));
 		Set<Class<? extends GeneratedMessageV3>> classes = reflections.getSubTypesOf(GeneratedMessageV3.class);
 		log.info("Found these proto classes:");
 		for (Class<? extends GeneratedMessageV3> cl : classes) {

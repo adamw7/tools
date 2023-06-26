99b8bbdbcbc645d1237706c578185117d4728797
adamw7 <adamwitkowski3@gmail.com>
2023-06-26 15:08:39 +0200

    Extending testing for generated code and removing old obsolete ones

--- code/protogen-maven-plugin-test/src/test/java/io/github/adamw7/tools/code/usecase/GeneretedCodeTest.java
+++ code/protogen-maven-plugin-test/src/test/java/io/github/adamw7/tools/code/usecase/GeneretedCodeTest.java
@@ -1,23 +1,36 @@
 package io.github.adamw7.tools.code.usecase;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import io.github.adamw7.tools.code.ComputerBuilder;
+import io.github.adamw7.tools.code.ComputerOptionalIfc;
 import io.github.adamw7.tools.code.test.Computer;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class GeneretedCodeTest {
 
-	
-	
 	@Test
 	public void happyPath() {
 		ComputerBuilder builder = new ComputerBuilder();
-		Computer computer = builder.setId(5).setName("Desktop").build();
+		ComputerOptionalIfc computerOptional = builder.setId(5);
+		assertNotNull(computerOptional);
+		Computer computer = computerOptional.setName("Desktop").build();
 		assertNotNull(computer);
 		assertEquals("Desktop", computer.getName());
 		assertEquals(5, computer.getId());
+		
+		assertTrue(computerOptional.hasName());
+		assertTrue(builder.hasId());
+		
+		computerOptional.clearName();
+		
+		assertFalse(computerOptional.hasName());
+				
+		assertEquals("", computerOptional.build().getName());
 	}
 }

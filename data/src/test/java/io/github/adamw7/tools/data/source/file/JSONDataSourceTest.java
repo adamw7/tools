cc21585d7fa5dfe5ed9b4a001923353c80d6ac76
adamw7 <adamwitkowski3@gmail.com>
2023-08-24 14:34:31 +0200

    Introducing parametrized tests for JSON data source in order to test both compressed and uncompressed files

--- data/src/test/java/io/github/adamw7/tools/data/source/file/JSONDataSourceTest.java
+++ data/src/test/java/io/github/adamw7/tools/data/source/file/JSONDataSourceTest.java
@@ -3,41 +3,43 @@ package io.github.adamw7.tools.data.source.file;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import io.github.adamw7.tools.data.Utils;
+import io.github.adamw7.tools.data.source.interfaces.IterableDataSource;
 
 public class JSONDataSourceTest {
-    private InMemoryJSONDataSource dataSource;
+    
+	static Stream<Arguments> dataSources() {
+		IterableDataSource zippedDataSource = new InMemoryJSONDataSource(Utils.getFileName("test.json.gz"));
+		IterableDataSource unzippedDataSource = new InMemoryJSONDataSource(Utils.getFileName("test.json"));
+		return Stream.of(of(Utils.named(zippedDataSource)), of(Utils.named(unzippedDataSource)));
+	}
 
-    @BeforeEach
-    public void setUp() {
-        dataSource = new InMemoryJSONDataSource(Utils.getFileName("test.json.gz"));
-        dataSource.open();
-    }
-
-    @AfterEach
-    public void tearDown() throws IOException {
-        dataSource.close();
-    }
-
-    @Test
-    public void testGetColumnNames() {
-        String[] columnNames = dataSource.getColumnNames();
+	@ParameterizedTest
+	@MethodSource("dataSources")
+    public void testGetColumnNames(InMemoryJSONDataSource source) throws IOException {
+		source.open();
+        String[] columnNames = source.getColumnNames();
         assertEquals(10, columnNames.length);
         assertArrayEquals(new String[]{"cars", "fruits", "year", "city", "name", "model", "state", "people", "age", "manufacturer"}, columnNames);
+        source.close();
     }
 
-    @Test
-    public void testFlattenedData() {
+	@ParameterizedTest
+	@MethodSource("dataSources")
+    public void testFlattenedData(InMemoryJSONDataSource source) throws IOException {
+		source.open();
         int rowCount = 0;
-        while (dataSource.iterator().hasNext()) {
-            String[] row = dataSource.nextRow();
+        while (source.iterator().hasNext()) {
+            String[] row = source.nextRow();
         	rowCount++;
 
         	assertEquals(2, row.length);
@@ -45,6 +47,7 @@ public class JSONDataSourceTest {
             assertNotNull(row[1]);
         }
         assertEquals(10, rowCount);
+        source.close();
     }
 }
 

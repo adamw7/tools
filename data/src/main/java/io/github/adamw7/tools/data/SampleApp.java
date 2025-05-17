1b65f445ed91b185593c47c69c45169cb285c240
adamw7 <adamwitkowski3@gmail.com>
2025-05-17 12:50:09 +0200

    Introducing a simple sample app for uniqueness check

---
+++ data/src/main/java/io/github/adamw7/tools/data/SampleApp.java
@@ -0,0 +1,54 @@
+package io.github.adamw7.tools.data;
+
+import java.io.FileNotFoundException;
+import java.io.UncheckedIOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import io.github.adamw7.tools.data.source.file.InMemoryCSVDataSource;
+import io.github.adamw7.tools.data.source.interfaces.InMemoryDataSource;
+import io.github.adamw7.tools.data.uniqueness.AbstractUniqueness;
+import io.github.adamw7.tools.data.uniqueness.InMemoryUniquenessCheck;
+import io.github.adamw7.tools.data.uniqueness.Result;
+
+public class SampleApp {
+	
+
+	private final static Logger log = LogManager.getLogger(SampleApp.class.getName());
+
+	public static void main(String[] args) {
+		checkArgs(args);
+		String fileName = args[0];
+		String columnName = args[1];
+		executeUniquenessSearch(fileName, columnName);
+	}
+
+	private static void executeUniquenessSearch(String fileName, String columnName) {
+		try {
+			InMemoryDataSource source = new InMemoryCSVDataSource(fileName, 1);
+			AbstractUniqueness check = new InMemoryUniquenessCheck();
+			
+			source.readAll();
+			
+			check.setDataSource(source);
+			print(check.exec(columnName), columnName);			
+		} catch (FileNotFoundException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+
+	private static void print(Result result, String column) {
+		if (result.isUnique()) {
+			log.info(column + " is unique");	
+		} else {
+			log.info(column + " is NOT unique");	
+		}
+	}
+
+	private static void checkArgs(String[] args) {
+		if (args == null || args.length <2) {
+			throw new IllegalArgumentException("Input format should be: filename columnname");
+		}
+	}
+}

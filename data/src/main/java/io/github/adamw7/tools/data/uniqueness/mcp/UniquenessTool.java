1095d5a9e8ac920033298b833da5735845081d14
adamw7 <adamwitkowski3@gmail.com>
2025-07-01 19:15:31 +0200

    Post proto MCP cleanup

---
+++ data/src/main/java/io/github/adamw7/tools/data/uniqueness/mcp/UniquenessTool.java
@@ -0,0 +1,50 @@
+package io.github.adamw7.tools.data.uniqueness.mcp;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import io.modelcontextprotocol.spec.McpSchema.CallToolResult;
+import io.modelcontextprotocol.spec.McpSchema.TextContent;
+import io.modelcontextprotocol.spec.McpSchema.Tool;
+
+public class UniquenessTool implements Function<Map<String, Object>, CallToolResult> {
+
+	private final Tool toolDefinition;
+
+	public UniquenessTool() {
+		this.toolDefinition = new Tool("uniqueness_check",
+				"Check if a given set of columns in unique in a given data set", """
+						{
+						    "type": "object",
+						    "properties": {
+						        "file": {
+						            "type": "string",
+						            "description": "filename"
+						        },
+						  		"columns_row": {
+						            "type": "int",
+						            "description": "number of the columns row"
+						        },
+						    },
+						    "required": ["data", ""columns_row""]
+						}
+						""");
+	}
+
+	public Tool getToolDefinition() {
+		return toolDefinition;
+	}
+
+	@Override
+	public CallToolResult apply(Map<String, Object> arguments) {
+		String result = runUniqueness(arguments);
+
+		return new CallToolResult(List.of(new TextContent(result)), false);
+	}
+
+	private String runUniqueness(Map<String, Object> arguments) {
+		return null;
+	}
+
+}

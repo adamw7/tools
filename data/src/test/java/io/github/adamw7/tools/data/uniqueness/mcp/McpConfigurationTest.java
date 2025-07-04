b790e44fa9a6705288a68506f5616ef089716be0
adamw7 <adamwitkowski3@gmail.com>
2025-07-04 15:37:39 +0200

    Introducing McpConfigurationTest

---
+++ data/src/test/java/io/github/adamw7/tools/data/uniqueness/mcp/McpConfigurationTest.java
@@ -0,0 +1,22 @@
+package io.github.adamw7.tools.data.uniqueness.mcp;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.modelcontextprotocol.server.McpSyncServer;
+import io.modelcontextprotocol.server.transport.StdioServerTransportProvider;
+
+public class McpConfigurationTest {
+
+    @Test
+    public void happyPath() {
+        McpConfiguration config = new McpConfiguration();
+        assertFalse(config.objectMapper() == null);
+        McpSyncServer server = config.mcpSyncServer(new StdioServerTransportProvider(new ObjectMapper()));
+        assertFalse(server.getServerCapabilities().tools() == null);
+        server.close();
+    }
+}

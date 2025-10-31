d574990f46975505c14428e11b84d0f1c900f47b
adamw7 <adamwitkowski3@gmail.com>
2025-10-31 11:17:57 +0100

    Introducing draft of context finder for context engineering purposes

---
+++ code/context/src/main/java/io/github/adamw7/context/ClassContainer.java
@@ -0,0 +1,5 @@
+package io.github.adamw7.context;
+
+public record ClassContainer(String className, String originalCode) {
+
+}
---
+++ code/context/src/main/java/io/github/adamw7/context/Finder.java
@@ -0,0 +1,59 @@
+package io.github.adamw7.context;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Finder {
+	private static final Logger log = LogManager.getLogger(Finder.class.getName());
+	private final Set<ClassContainer> allContainers;
+
+	public Finder(Set<ClassContainer> allContainers) {
+		this.allContainers = allContainers;
+	}
+
+	public Set<ClassContainer> find(ClassContainer root, int depth) {
+		Set<ClassContainer> classes = findAllUsedClasses(root, depth);
+		for (int i = depth - 1; i > 0; i--) {
+			for (ClassContainer clazz : classes) {
+				classes.addAll(find(clazz, i));
+			}
+		}
+		return classes;
+	}
+
+	private Set<ClassContainer> findAllUsedClasses(ClassContainer root, int depth) {
+		if (depth <= 0) {
+			throw new IllegalArgumentException("Depth must be positive and received: " + depth);
+		}
+		Pattern pattern = Pattern.compile("\\b[A-Z][A-Za-z0-9_]*(\\.[A-Z][A-Za-z0-9_]*)*\\b");
+		return findByRegEx(root, pattern);
+	}
+
+	private Set<ClassContainer> findByRegEx(ClassContainer root, Pattern pattern) {
+		Set<ClassContainer> classes = ConcurrentHashMap.newKeySet();
+		Matcher matcher = pattern.matcher(root.originalCode());
+		while (matcher.find()) {
+			String className = matcher.group();
+			ClassContainer container = findContainer(className);
+			if (container != null) {
+				log.info("Found {} used in {}", container.className(), root.className());
+				classes.add(container);				
+			}
+		}
+		return classes;
+	}
+
+	private ClassContainer findContainer(String className) {
+		for (ClassContainer container : allContainers) {
+			if (container.className().equals(className + ".java")) {
+				return container;
+			}
+		}
+		return null;
+	}
+}
